b4b8393110678037a7b21bbc0e5fd5f5f2112ea2
Michail Plushnikov <michail@plushnikov.de>
2015-03-01 11:50:04 +0100

    renamed file

--- test-manual/lombok/src/main/java/de/plushnikov/builder/simple/BuilderAtConstructorSimplePRedefined.java
+++ test-manual/lombok/src/main/java/de/plushnikov/builder/simple/BuilderAtConstructorSimplePRedefined.java
@@ -2,19 +2,19 @@ package de.plushnikov.builder.simple;
 
 public class BuilderAtConstructorSimplePredefined {
     private int myInt;
-    private String myString;
+    private String myString2;
 
     @lombok.Builder
-    public BuilderAtConstructorSimplePredefined(int myInt, String myString) {
+    public BuilderAtConstructorSimplePredefined(int myInt, String myString2) {
         this.myInt = myInt + 1;
-        this.myString = myString;
+        this.myString = myString2;
     }
 
     static class BuilderAtConstructorSimplePredefinedBuilder {
         private int myInt;
 
-        public BuilderAtConstructorSimplePredefined.BuilderAtConstructorSimplePredefinedBuilder myString(String myString) {
-            this.myString = myString + "something";
+        public BuilderAtConstructorSimplePredefined.BuilderAtConstructorSimplePredefinedBuilder myString2(String myString) {
+            this.myString2 = myString + "something";
             return this;
         }
     }
